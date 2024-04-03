991a6f1620aa5c989f609ed9f6958b3571ca70c4
ratnalasreenivas <sreenu96123@gmail.com>
2024-04-02 20:20:57 -0500

    HERE I HAVE ADDED TWO SOP LINES OF CODE

--- Display.java
+++ Display.java
@@ -3,5 +3,8 @@ class Display
 	public static void main(String args[])
 	{
 		System.out.println("Welcome Devops Team");
+		System.out.println("This is first lane");
+		System.out.println("This is Second lane");
 	}
+
 }
