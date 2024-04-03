7d665999884b4629ef877796b5a6bea20ad9cc0a
ratnalasreenivas <136493196+ratnalasreenivas@users.noreply.github.com>
2024-04-02 21:28:16 -0500

    Added some lines of code

--- Dis.java
+++ Dis.java
@@ -3,6 +3,10 @@ class Dis
 	public static void main (String args[])
 	{
 		System.out.println("Welcome to Devops Classes");
+		System.out.println("Welcome to Devops Classes by Fayaz");
+		System.out.println("Welcome to Devops Classes by Rama Krishna");
+		System.out.println("Welcome to Devops Classes and it is located in INDIA");
+		System.out.println("Welcome to Devops Classes reffered by Anji Bava");
 	}
 }
 
